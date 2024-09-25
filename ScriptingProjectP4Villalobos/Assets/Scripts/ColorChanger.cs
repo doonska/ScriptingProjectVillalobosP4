cfcfa2e50ff22f0b512a56587ec376ef80184bcf
doonska <727187@student.dusd.net>
2024-09-25 11:21:42 -0700

    Did Scripting Assignment
    I got the cube to change colors.

---
+++ ScriptingProjectP4Villalobos/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        { GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        
+       
+        
+    
+    

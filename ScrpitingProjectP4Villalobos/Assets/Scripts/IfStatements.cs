7d93c87c75739ba252f3b8444690162293d5b6f5
doonska <727187@student.dusd.net>
2024-10-28 10:44:37 -0700

    ses
    dfd

---
+++ ScrpitingProjectP4Villalobos/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,38 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f
+        float hotLimitTemperature = 70.0f
+        float coldLimitTemperature = 40.0f
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Temperaturetest();
+
+        coffeeTemperature -= Time.deltaTime; *5f;
+
+    }
+    void TemperatureTest()
+    {
+        //If the coffee's temperature is greater than the hottest drinking temperature.
+        if (coffeeeTemperature > hotLimitTemperature)
+    }
+
+    {
+
+    
+
+
+
+
